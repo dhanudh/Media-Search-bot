c2081f8415f2b7b5edc8c4c5d31e9db714ca9a15
dhanudh <77167838+dhanudh@users.noreply.github.com>
2021-08-06 18:52:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,20 +8,6 @@ from utils import Media
 logger = logging.getLogger(__name__)
 
 
-@Client.on_message(filters.command('start'))
-async def start(bot, message):
-    """Start command handler"""
-    if len(message.command) > 1 and message.command[1] == 'subscribe':
-        await message.reply(INVITE_MSG)
-    else:
-        buttons = [[
-            InlineKeyboardButton('Search Here', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('Go Inline', switch_inline_query=''),
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(START_MSG, reply_markup=reply_markup)
-
-
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
     """Send basic information of channel"""
